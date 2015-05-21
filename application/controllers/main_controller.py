08bbee3f5c2026ac8cb8b8ecf54eebb936e61ef1
Kiriychuk <Kirias@ukr.net>
2015-05-21 19:26:48 +0300

    corecting pages

--- application/controllers/main_controller.py
+++ application/controllers/main_controller.py
@@ -16,34 +16,7 @@ class Main(object):
 		self.user = User()
 
 	def show(self):
-		# data = self.get_list_all_user() # default show old list user
 		data = self.user.get_list_all_user()
 		return self.__view.render(data)
-
-
-
-	# def add_person(self,request):
-	# 	if request.method == "POST":
-	# 		if 'button_person' in request.form:
-	# 			if (len(request.form["name_new_person"]) != 0 and len(request.form["surname_new_person"]) != 0):
-	# 				name_person = request.form["name_new_person"]
-	# 				surname_person = request.form["surname_new_person"]
-	# 				self.model.AddNewPerson(name_person,surname_person)
-
-	# def delete_person(self, request):
-	# 	drop_index = request.args.get("drop")
-	# 	self.model.DropPerson(drop_index)
-
-
-	# def user_auth(self, request):
-	# 	if 'username' in request.form and 'password' in request.form:
-	# 		if self.model.IsExistsUser('Users',request.form['username'], request.form['password']):
-	# 			session["username"] = request.form['username']
-	# 			session["password"] = request.form['password']
-	# 			self.__session = session
-	# 			return redirect(url_for('index'))
-
-
-
 
 
